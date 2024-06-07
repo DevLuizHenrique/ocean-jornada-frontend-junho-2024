221e43d180fb5342e0db1c4f8771da625d7e28fd
Luiz Henrique <126506975+DevLuizHenrique@users.noreply.github.com>
2024-06-07 14:31:30 +0000

    adicionar novo item

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,15 @@
-export default function Card() {
+export default function Card(props) {
+    const item = props.item;
+
     return (
     <div className="card">
-    <h2>Rick Sanchez</h2>
+    <h2>{ item.name }</h2>
     <div className="tags">
       <div>Status: Vivo</div>
       <div>Espécie: Humana</div>
       <div>Origem: Terra C-137</div>
     </div>
-    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" className="character-image" />
+    <img src={ item.image}/>
     <div className="additional-info">Informação adicional</div>
   </div>
   )
