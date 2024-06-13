052021e44f2069ea09860bcce139e265ae32b56f
Luiz Henrique <126506975+DevLuizHenrique@users.noreply.github.com>
2024-06-13 14:45:46 +0000

    formatando o codigo

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import Card from './components/Card/Card'
 
 function App() {
   const [itens, setItens] = useState([]);
-  
+
   async function carregarDados() {
     const apiUrl = 'https://rickandmortyapi.com/api/character';
+
     const response = await fetch(apiUrl);
+
     const body = await response.json();
+
     const results = body.results;
+
     console.log(results);
 
     setItens(results);
   }
-   useEffect(() => {carregarDados()}, []);
+  useEffect(() => { carregarDados() }, []);
 
   return (
     <>
