68abd47a8df27688688c864491f10a183df8da90
Luiz Henrique <126506975+DevLuizHenrique@users.noreply.github.com>
2024-06-14 14:59:55 +0000

    ajustando codigo

--- src/pages/ViewItemById.jsx
+++ src/pages/ViewItemById.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import './ViewItemById.css'
 
 function ViewItemById() {
   const [item, setItem] = useState()
@@ -26,9 +27,44 @@ function ViewItemById() {
 
   return (
     <>
-    <div className='view_item'></div>
-    <link to="/">Voltar para Home</link>
-      <h1>{item.name}</h1>
+      <div className="view_item">
+        <Link to="/">Voltar para Home</Link>
+
+        <h1>{item.name}</h1>
+
+        <div className="content">
+          <div>
+            <img src={item.image} />
+          </div>
+
+          <div className="attributes">
+            <div className="attribute">
+              <h2>STATUS</h2>
+              <p>{item.status}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>SPECIES</h2>
+              <p>{item.species}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>GENDER</h2>
+              <p>{item.gender}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>ORIGIN</h2>
+              <p>{item.origin.name}</p>
+            </div>
+
+            <div className="attribute">
+              <h2>LOCATION</h2>
+              <p>{item.location.name}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
